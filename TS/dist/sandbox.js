072211e07dd7f3abf758fbc852d82fd151be320a
thatFemicode <leyedavid10@gmail.com>
2022-05-17 23:31:17 +0100

    Type assertion

--- TS/dist/sandbox.js
+++ TS/dist/sandbox.js
@@ -9,7 +9,7 @@ let x = false;
 // The following are called Tuples
 let person = [1, "Brad", true];
 // let me = ;
-console.log(person[person.length - 2]);
+// console.log(person[person.length - 2]);
 // Tuples Array
 let employees;
 employees = [
@@ -17,4 +17,30 @@ employees = [
     [2, "John"],
     [3, "Jill"],
 ];
-console.log(employees);
+console.log(employees[0][0]);
+// Union
+let pid;
+pid = "22";
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: "John",
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
--- TS/src/sandbox.ts
+++ TS/src/sandbox.ts
@@ -10,7 +10,7 @@ let x: any = false;
 // The following are called Tuples
 let person: [number, string, boolean] = [1, "Brad", true];
 // let me = ;
-console.log(person[person.length - 2]);
+// console.log(person[person.length - 2]);
 
 // Tuples Array
 let employees: [number, string][];
@@ -20,4 +20,38 @@ employees = [
   [2, "John"],
   [3, "Jill"],
 ];
-console.log(employees);
+console.log(employees[0][0]);
+// Union
+let pid: string | number;
+pid = "22";
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: "John",
+};
+
+// Type Assertion
+let cid: any = 1;
+// let customerId = <number>cid
+let customerId = cid as number;
